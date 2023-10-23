160620335cc75c1248883ab872de080b3ce47726
Deramaux vincent <73826001+VinceDeCompiegne@users.noreply.github.com>
2023-10-23 15:30:11 +0200

    Update poo.authManager.ts

--- assets/js/POO/poo.authManager.ts
+++ assets/js/POO/poo.authManager.ts
@@ -15,7 +15,7 @@ class AuthenticationManager {
         this.editionBoutton = document.querySelector(".edition-boutton");
         this.editionBondeau = document.querySelector(".edition-bondeau");
         this.filtre = document.querySelector(".filtre");
-        this.add = 'localhost:5678';
+        this.add = 'oc-p8-kasa.vincent-deramaux-portfolio.fr';
 
         if (this.formulaireConnexion) {
             this.setupLoginForm();
